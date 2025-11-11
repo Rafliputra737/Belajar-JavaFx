260c5d7eddb480f11946e9d149755aa564c18337
Rafli Putra Permana <permanarafli686@gmail.com>
2025-11-11 17:39:18 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author LENOVO LOQ
+ */
+
+
+import java.io.File;
+import java.net.URL;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        // Pastikan path FXML sesuai dengan lokasi Anda
+        URL url= new File ("src/main/java/View/Beranda.fxml").toURI().toURL();
+        Scene s = new Scene (FXMLLoader.load(url));
+    // show stage
+    stage.setScene(s);
+    stage.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+    
+    
+
